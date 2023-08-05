fe0d584bbb1f2cdb70af3e48d4341e3ac5a55ebd
NTDHaruto <dung11112003qng@gmail.com>
2023-08-05 21:11:18 +0700

    Bai 65 (3.8.2023)
    Bài 65: Viết chương trình thực hiện những yêu cầu sau:
    a. Tạo mảng một chiều n phần tử nguyên có giá trị nhập vào từ bàn phím
    b. Tính trung bình cộng của các số nguyên âm lẻ có trong mảng.
    c. Xóa các phần tử có trị trùng nhau trong mảng, chỉ chừa lại một phần tử

---
+++ Bai 65/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Collections.Generic;
+
+namespace Bai_65
+{
+    internal class Program
+    {
+        static void XoaTrung(int[] A)
+        {
+            Console.WriteLine();
+            HashSet<int> set = new HashSet<int>(A);
+            int[] temp = set.ToArray();
+            foreach (int i in temp)
+            {
+                Console.Write(i+ " ");
+            }
+            Console.WriteLine();
+        }
+
+        static double TBC(int[] A)
+        {
+            double sum = 0;
+            int cnt = 0;
+            foreach (int i in A)
+            {
+                if (i < 0)
+                {
+                    cnt++;
+                    sum += i;
+                }
+            }
+            return sum/cnt;
+        }
+
+        static int[] TaoMang(out int n)
+        {
+            Console.Write("Nhap so luong phan tu: ");
+            n = int.Parse(Console.ReadLine());
+            int[] A = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("Nhap phan tu thu {0}: ", i + 1);
+                A[i] = int.Parse(Console.ReadLine());
+            }
+            return A;
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+            int[] A = TaoMang(out n);
+            Console.Write("Trung binh cong cac so nguyen am le = {0}", TBC(A));
+            XoaTrung(A);
+        }
+    }
+}
