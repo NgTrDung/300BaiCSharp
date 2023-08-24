0eaeaea8a86daf474e5bc25f96e6fd5bb4405055
NTDHaruto <dung11112003qng@gmail.com>
2023-08-24 20:37:09 +0700

    Bài 68 (24.8.2023)
    Bài 68: Viết chương trình thực hiện những yêu cầu sau:
    a. Tạo ngẫu nhiên mảng một chiều n phần tử nguyên (n chẵn) có giá trị chứa trong đoạn [100, 200] và xuất mảng.
    b. Chia các phần tử của mảng thành hai nhóm, sao cho hiệu của tổng các phần tử nhóm này và tổng các phần tử nhóm kia là một số dương nhỏ nhất.
    Tìm cặp a0, b0 (a0 > b0) có hiệu nhỏ nhất, cặp a1, b1 (a1 > b1) có hiệu nhỏ thứ hai, … Như vậy hiệu (a0 + a1 + …) - (b0 + b1 + …) sẽ nhỏ nhất.
    Tham khảo thêm bài 62, trang 19.

---
+++ Bai 68/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace MinDifference
+{
+    class Program
+    {
+        const int MAX = 100;
+
+        static void Swap(ref int a, ref int b)
+        {
+            int t = a;
+            a = b;
+            b = t;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] a = new int[MAX];
+            int n, i, j, i1=0, j1=0, k;
+            Random random = new Random();
+
+            do
+            {
+                Console.Write("Nhap n (n chan): ");
+                n = int.Parse(Console.ReadLine());
+            } while (n < 2 || n % 2 != 0);
+
+            for (i = 0; i < n; ++i)
+            {
+                a[i] = random.Next(100) + 100;
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+
+            for (k = 0; k < n / 2; ++k)
+            {
+                int min = 101;
+                for (i = k; i < n - k; ++i)
+                {
+                    for (j = i + 1; j < n - k; ++j)
+                    {
+                        if (Math.Abs(a[i] - a[j]) < min)
+                        {
+                            min = Math.Abs(a[i] - a[j]);
+                            i1 = i;
+                            j1 = j;
+                        }
+                    }
+                }
+
+                if (a[i1] < a[j1])
+                    Swap(ref a[i1], ref a[j1]);
+
+                Swap(ref a[i1], ref a[k]);
+                Swap(ref a[j1], ref a[n - k - 1]);
+            }
+
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for (i = 0; i < n / 2; ++i)
+            {
+                Console.Write(a[i] + " ");
+                sum1 += a[i];
+            }
+            Console.Write(": " + sum1 + "\n");
+
+            for (i = n / 2; i < n; ++i)
+            {
+                Console.Write(a[i] + " ");
+                sum2 += a[i];
+            }
+            Console.Write(": " + sum2 + "\n");
+
+            Console.WriteLine("Hieu nho nhat = " + (sum1 - sum2));
+        }
+    }
+}
